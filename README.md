7d23bc6458f3eb2318a3cdf87a3cd1820c0309cd
ubuntupunk@gmail.com <ubuntupunk@gmail.com>
2025-01-06 10:36:57 +0200

    added README

---
+++ README.md
@@ -0,0 +1,41 @@
+# Ulauncher Vim Commands
+
+A Ulauncher extension that provides quick access to Vim commands with descriptions and links to detailed documentation.
+
+## Features
+
+- Quick search through Vim commands
+- Command descriptions and usage
+- Direct links to vim.rtorr.com for detailed documentation
+- Fuzzy search support for both command names and descriptions
+
+## Installation
+
+1. Open Ulauncher preferences
+2. Go to "Extensions" tab
+3. Click "Add extension"
+4. Paste the following URL: https://github.com/ubuntupunk/ulauncher-vim
+
+
+## Usage
+
+1. Open Ulauncher (Ctrl+Space)
+2. Type your extension keyword (default: "vm")
+3. Start typing to search for Vim commands
+4. Press Enter on a command to view detailed documentation
+
+## Development
+
+This extension is built using:
+- Python 3
+- Ulauncher API
+- vim.rtorr.com command database
+
+## Credits
+
+- Command data sourced from vim.rtorr.com
+- Icon from [source of your icon]
+
+## License
+
+MIT License
--- main.py
+++ main.py
@@ -4,24 +4,50 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
-
+from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
+import json
+import os
 
 class VmExtension(Extension):
 
+    def load_vim_commands(self):
+        """Load Vim commands from JSON file."""
+        package_dir = os.path.dirname(os.path.abspath(__file__))
+        full_path = os.path.join(package_dir, 'db', 'commands.json')   
+        with open(full_path, 'r') as file:
+            return json.load(file)
+
     def __init__(self):
         super().__init__()
+        self.vim_commands = self.load_vim_commands()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-
 
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
+        query = event.get_argument() or ""
         items = []
-        for i in range(5):
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name='Item %s' % i,
-                                             description='Item description %s' % i,
-                                             on_enter=HideWindowAction()))
+
+        # If no query, show all commands (limited to first 8)
+        commands_to_show = extension.vim_commands
+        
+        # If there's a query, filter commands
+        if query:
+            commands_to_show = [
+                cmd for cmd in extension.vim_commands
+                if query.lower() in cmd['command'].lower() or 
+                   query.lower() in cmd['description'].lower()
+            ]
+
+        # Limit results to first 8 matches
+        for cmd in commands_to_show[:8]:
+            url = f"https://vim.rtorr.com/#:~:text={cmd['rtorr_description']}"
+            items.append(ExtensionResultItem(
+                icon='images/icon.png',
+                name=cmd['command'],
+                description=f"{cmd['name']} - {cmd['description']}",
+                on_enter=OpenUrlAction(url)
+            ))
 
         return RenderResultListAction(items)
 
